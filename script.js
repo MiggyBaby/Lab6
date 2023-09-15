ad341a64b5bfe75568cb2a95b9cfae94e256f7d0
MiggyBaby <142875260+MiggyBaby@users.noreply.github.com>
2023-09-15 19:14:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const form = document.getElementById("myForm")
+        
+        form.addEventListener("submit", function(event) {
+            event.preventDefault(); 
 
+            const firstName = document.getElementById("firstName").value;
+            const middleInitial = document.getElementById("middleInitial").value;
+            const lastName = document.getElementById("lastName").value;
+            const email = document.getElementById("email").value;
+
+            if (!firstName || !middleInitial || !lastName || !isValidEmail(email)) {
+                alert("Invalid inputs. Please check your entries.");
+            } else {
+                alert("Success!");
+                form.reset(); 
+            }
+        });
+        
+        function isValidEmail(email) {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        }
